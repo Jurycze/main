92709704344227195900675c2bd4c4920e5eafac
Jurycze <94159801+Jurycze@users.noreply.github.com>
2023-03-18 22:20:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function submitForm() {
   var request = new XMLHttpRequest();
-  request.open("POST", "https://discord.com/api/webhooks/1081617393870196776/fDKBO8XvXcwAJE5EBNth6OghBVBPJzqJ_0vcVoh9m_Tm-rdtaBd0qELCxAbACE5e1r3h", true);
+  request.open("POST", "https://discord.com/api/webhooks/1086760712266846331/bIsrqitbZZJo8NyghGeRRRhHrNj-wNOb2lLg_yFNpaQJNgE4Vhifgh3JgnSI1bPUTbhB", true);
   request.setRequestHeader('Content-type', 'application/json');
 
   var inputText = document.getElementById("inputText").value;
