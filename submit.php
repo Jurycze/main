104b8e85702b3aaa0e2f9d4fae8d06496af34ace
Jurycze <94159801+Jurycze@users.noreply.github.com>
2023-03-20 13:18:09 +0100

    Update submit.php

--- submit.php
+++ submit.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   // Set up cURL to send the message to the webhook
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, 'https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN');
+  curl_setopt($ch, CURLOPT_URL, 'https://discord.com/api/webhooks/1087346349256745000/vy5YmbTW8u-eY-jPNFsMhh1MKKTlrdc_32CSX4hOffMavDTOK-8kQZClqEw5Jl-fUEW3');
   curl_setopt($ch, CURLOPT_POST, 1);
   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('content' => $message)));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
