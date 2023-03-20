e12148ef9ae239bbb6474d94cfca372fb6b8762d
Jurycze <94159801+Jurycze@users.noreply.github.com>
2023-03-20 13:12:38 +0100

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,13 +1,31 @@
 <?php
-if($_POST){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $inputText = $_POST['inputText'];
 
-    $to = "jajatuz2008@gmail.com";
-    $subject = "Nový návrh na webu";
-    $body = "Jméno: $name\nEmail: $email\nNávrh: $inputText";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Get the form data
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $inputText = $_POST['inputText'];
 
-    mail($to, $subject, $body);
+  // Construct the message to send to Discord
+  $message = "New message from $name ($email):\n$inputText";
+
+  // Set up cURL to send the message to the webhook
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, 'https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN');
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('content' => $message)));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+  // Send the message to the webhook
+  $response = curl_exec($ch);
+
+  // Close cURL
+  curl_close($ch);
+
+  // Redirect the user back to the form page
+  header('Location: index.html');
+  exit();
 }
+
 ?>
